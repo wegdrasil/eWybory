6390ca30cd80702f8ad8f977fd320a360c4aa3d1
Rafal Romanowski <wegdrasil@gmail.com>
2014-05-31 14:03:04 +0200

    Datebase is accesible in admin

--- Poll/admin.py
+++ Poll/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Poll, Answer, Vote
 
 class UserAdmin(admin.ModelAdmin):
     class Meta:
         model = User
 
+class PollAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Poll
+
+class AnswerAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Answer
+
+class VoteAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Vote
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Vote, VoteAdmin)
--- Poll/models.py
+++ Poll/models.py
@@ -1,38 +1,5 @@
 from django.db import models
 
-#class User(models.Model):
-    #login = models.CharField(max_length=25)
-    #password = models.CharField(max_length=255)
- #   email = models.EmailField()
- #   first_name = models.CharField(max_length=120, null=True, blank=True)
- #   last_name = models.CharField(max_length=120, null=True, blank=True)
- #   timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
- #   updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-#
- #   def __str__(self):
- #       return str(self.email)
-
-#class Poll(models.Model):
-#    question      = models.CharField(max_length = 255)
-#
-#    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-#    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-#
-#    #date_creation = models.DateField()
-#    #date_ending   = models.DateField()
-#
-#    def __unicode__(self):
-#        return smart_unicode(self.question)
-#
-#class Answer(models.Model):
-#    answer = models.CharField(max_length=255)
-#    votes = models.IntegerField()
-#
-#    def __unicode__(self):
-#        return smart_unicode(self.answer)
-
-
-
 class User(models.Model):
     login = models.CharField(max_length=25, null=False, blank=False)
     password = models.CharField(max_length=255, null=False, blank=False)
@@ -55,7 +22,7 @@ class Poll(models.Model):
 class Answer(models.Model):
     first_name = models.CharField(max_length=120, null=False, blank=False)
     last_name = models.CharField(max_length=120, null=False, blank=False)
-    poll  = models.ForeignKeyey(Poll)
+    poll  = models.ForeignKey(Poll)
     n_o_votes = models.IntegerField()
 
 
@@ -66,5 +33,8 @@ class Vote(models.Model):
     poll  = models.ForeignKey(Poll)
     answer = models.ForeignKey(Answer)
     user  = models.ForeignKey(User)
+
+    def __str__(self):
+        return str(self.id)
 
 
